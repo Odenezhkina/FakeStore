d2766d42fda88d12b53eedc7d975d5165c49d48d
Odenezhkina <66617436+Odenezhkina@users.noreply.github.com>
2022-09-21 15:14:03 -0700

    All is merged and fixed

--- app/src/main/java/com/example/fakestore/MainActivity.kt
+++ app/src/main/java/com/example/fakestore/MainActivity.kt
@@ -2,30 +2,19 @@ package com.example.fakestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.fakestore.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fakestore.databinding.ActivityMainBinding
-import com.example.fakestore.epoxy.UiProductEpoxyController
-import com.example.fakestore.model.ui.UiProduct
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
+//    private val viewModel: MainViewModel by lazy {
+//        ViewModelProvider(this)[MainViewModel::class.java]
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,28 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
-        
-        val epoxyController = UiProductEpoxyController(resources, viewModel)
-        // setting an empty state for shimmer ??
-        epoxyController.setData(emptyList())
-
-
-        combine(viewModel.store.stateFlow.map { it.products },
-            viewModel.store.stateFlow.map { it.favorites }) { listProducts, listFavorites ->
-            listProducts.map { product ->
-                UiProduct(product = product, isInFavorites = listFavorites.contains(product.id))
-            }
-        }.distinctUntilChanged().asLiveData().observe(this@MainActivity) { products ->
-            epoxyController.setData(products)
-        }
-        viewModel.refreshProducts()
-
-        with(binding) {
-            val itemDecorator =
-                DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL)
-            rvProducts.addItemDecoration(itemDecorator)
-            rvProducts.setController(epoxyController)
-        }
 
 
         // ui click listener
--- app/src/main/java/com/example/fakestore/fragments/ProductListFragment.kt
+++ app/src/main/java/com/example/fakestore/fragments/ProductListFragment.kt
@@ -6,15 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fakestore.MainViewModel
 import com.example.fakestore.R
 import com.example.fakestore.databinding.FragmentProductListBinding
-import com.example.fakestore.epoxy.ProductEpoxyController
+import com.example.fakestore.epoxy.UiProductEpoxyController
 import com.example.fakestore.model.mapper.ProductMapper
+import com.example.fakestore.model.ui.UiProduct
 import com.example.fakestore.network.NetworkService
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -41,18 +49,29 @@ class ProductListFragment : Fragment(R.layout.fragment_product_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val epoxyController = ProductEpoxyController(resources)
+        val mainViewModel: MainViewModel by viewModels()
+        val epoxyController = UiProductEpoxyController(resources, mainViewModel)
+        // setting an empty state for shimmer ??
+        epoxyController.setData(emptyList())
 
-        lifecycleScope.launchWhenStarted {
-            val response = service.getAllProducts()
-            val domainProductList = response.body()?.let { listNetworkProducts ->
-                listNetworkProducts.map {
-                    Log.d("TAG", it.toString())
-                    productMapper.buildFrom(networkProduct = it)
-                }
+        combine(mainViewModel.store.stateFlow.map { it.products },
+            mainViewModel.store.stateFlow.map { it.favorites }) { listProducts, listFavorites ->
+            listProducts.map { product ->
+                UiProduct(product = product, isInFavorites = listFavorites.contains(product.id))
             }
-            epoxyController.setData(domainProductList)
+        }.distinctUntilChanged().asLiveData().observe(viewLifecycleOwner) { products ->
+            epoxyController.setData(products)
         }
+        mainViewModel.refreshProducts()
+//        lifecycleScope.launchWhenStarted {
+//            val response = service.getAllProducts()
+//            val domainProductList = response.body()?.let { listNetworkProducts ->
+//                listNetworkProducts.map {
+//                    productMapper.buildFrom(networkProduct = it)
+//                }
+//            }
+//            epoxyController.setData(domainProductList)
+//        }
 
         with(binding) {
             val itemDecorator =
